f036d4d97f6750a971df253ee2c64ce6d8997d28
cfolliet <cfolliet@talentsoft.com>
2021-05-29 22:29:12 +0200

    remove jira js and fix filtersApi

--- src/routes/filtersApi.js
+++ src/routes/filtersApi.js
@@ -1,10 +1,9 @@
-const { Version2Client } = require("jira.js");
+const fetch = require('node-fetch');
 
 export async function get(req, res, next) {
     const { host, email, token } = req.query;
 
-    const client = getJiraClient(host, email, token);
-    const filters = await getFilters(client);
+    const filters = await getFilters(host, email, token);
 
     if (filters !== null) {
         res.setHeader('Content-Type', 'application/json');
@@ -14,19 +13,22 @@ export async function get(req, res, next) {
     }
 }
 
-async function getFilters(client) {
-    let result = await client.filters.getFiltersPaginated({ filterName: 'guesstimate', expand: 'jql' })
-    return result.values;
-}
-
-function getJiraClient(host, email, token) {
-    return new Version2Client({
-        host: host,
-        authentication: {
-            basic: {
-                username: email,
-                apiToken: token
+async function getFilters(host, email, token) {
+    let response;
+    try {
+        response = await fetch(host + '/rest/api/2/filter/search?filterName=guesstimate&expand=jql', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${Buffer.from(
+                    email + ':' + token
+                ).toString('base64')}`,
+                'Accept': 'application/json'
             }
-        }
-    });
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+    const data = await response.json();
+    return data.values;
 }
